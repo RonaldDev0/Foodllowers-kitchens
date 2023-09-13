6c998e3c5292c8d3cb6df055bb3e20df51da9f0b
Ronald <ronaldzamoradev@gmail.com>
2023-09-13 14:48:42 -0500

    setup

---
+++ src/app/providers.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { NextUIProvider } from '@nextui-org/react'
+import type { ReactNode } from 'react'
+
+export function Providers ({ children }: { children: ReactNode }) {
+  return (
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
+  )
+}
