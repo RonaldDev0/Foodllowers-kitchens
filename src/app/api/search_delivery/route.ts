f13efdd65802ee66a28f0297487eed5ce94eb99a
Ronald <ronaldzamoradev@gmail.com>
2024-06-04 10:58:10 -0500

    feat: api/search_delivery

--- src/app/api/search_delivery/route.ts
+++ src/app/api/search_delivery/route.ts
@@ -1,8 +1,9 @@
 /* eslint-disable camelcase */
 import { NextRequest, NextResponse } from 'next/server'
-import { updateOrderWithDeliveryID } from './updateOrderWithDeliveryID'
-import { updateDeliveryStatus } from './updateDeliveryStatus'
-import { searchMostCloseDelivery } from './searchMostCloseDelivery'
+import { createClient } from '@supabase/supabase-js'
+import { calculateHaversineDistance } from './calculateHaversineDistance'
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
 
 export async function POST (req: NextRequest) {
   const { kitchenAddress, orderID } = await req.json()
@@ -10,12 +11,54 @@ export async function POST (req: NextRequest) {
     return NextResponse.json({ error: 'kitchenAddress or orderID is missing' })
   }
 
-  const { delivery_id, error } = await searchMostCloseDelivery(kitchenAddress)
-  if (error || !delivery_id) {
-    return NextResponse.json({ error: 'delivery not found' })
-  }
-  updateOrderWithDeliveryID(orderID, delivery_id)
-  updateDeliveryStatus(delivery_id)
+  // get delivery id
+  const { delivery_id, error } = await supabase
+    .from('deliverys')
+    .select('id, current_location')
+    .match({ register_complete: true, active: true, free: true })
+    .then(({ data, error }) => {
+      if (error) return { error }
+       if (data.length === 0) return { error: 'no delivery found' }
 
-  return NextResponse.json({ success: true })
+      let closestDeliveryId = ''
+      let shortestDistance = Infinity
+
+      data.forEach(({ id, current_location }) => {
+        const distance = calculateHaversineDistance(kitchenAddress, current_location)
+        if (distance < shortestDistance) {
+          shortestDistance = distance
+          closestDeliveryId = id
+        }
+      })
+
+      return { delivery_id: closestDeliveryId, error: false }
+    })
+
+  if (error || !delivery_id) {
+    return NextResponse.json({ error, success: false })
+  }
+
+
+  // update order state
+  const { error: updateOrderError } = await supabase
+    .from('orders')
+    .update({ delivery_id, order_state: 'buscando delivery...' })
+    .eq('id', orderID)
+
+  if (updateOrderError) {
+    return NextResponse.json({ error: updateOrderError, success: false })
+  }
+
+
+  // update delivery state
+  const { error: updateDeliveryError } = await supabase
+    .from('deliverys')
+    .update({ free: false })
+    .eq('id', delivery_id)
+
+  if (updateDeliveryError) {
+    return NextResponse.json({ error: updateDeliveryError, success: false })
+  }
+
+  return NextResponse.json({ error: false })
 }
