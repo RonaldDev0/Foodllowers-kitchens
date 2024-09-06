8b71782af3e4939e02628107e7860c9e991f9a41
Ronald <ronaldzamoradev@gmail.com>
2024-09-06 14:08:05 -0500

    feat: upate register (bank account complete)

--- src/app/register/Components/BankAccount.tsx
+++ src/app/register/Components/BankAccount.tsx
@@ -3,93 +3,59 @@ import { useState } from 'react'
 import { Select, SelectItem, Input, Button } from '@nextui-org/react'
 import { useData } from '@/store'
 import { useSupabase } from '@/app/providers'
-
-const banks = [
-  {
-    name: 'Bancolombia',
-    value: 'Bancolombia'
-  },
-  {
-    name: 'Banco Caja Social',
-    value: 'Banco Caja Social'
-  },
-  {
-    name: 'Banco Davivienda',
-    value: 'Banco Davivienda'
-  },
-  {
-    name: 'Banco de Bogotá',
-    value: 'Banco de Bogotá'
-  },
-  {
-    name: 'Banco de la República',
-    value: 'Banco de la República'
-  },
-  {
-    name: 'Banco de Occidente',
-    value: 'Banco de Occidente'
-  },
-  {
-    name: 'BBVA Colombia',
-    value: 'BBVA Colombia'
-  },
-  {
-    name: 'Banco Pichincha',
-    value: 'Banco Pichincha'
-  },
-  {
-    name: 'Banco Santander Colombia',
-    value: 'Banco Santander Colombia'
-  },
-  {
-    name: 'Financiera Comultrasan',
-    value: 'Financiera Comultrasan'
-  },
-  {
-    name: 'Itaú Corpbanca Colombia',
-    value: 'Itaú Corpbanca Colombia'
-  }
-]
+import { banks } from './banks'
 
 export function BankAccount ({ onClose }: { onClose: any }) {
   const { kitchenId, kitchen, setStore } = useData()
   const { supabase } = useSupabase()
 
+  const [accountType, setAccountType] = useState<any>(kitchen ? kitchen?.bank_account?.accountType : '')
+  const [accountTypeError, setAccountTypeError] = useState<any>(null)
+
   const [bank, setBank] = useState<any>(kitchen ? kitchen?.bank_account?.bank : null)
   const [bankError, setBankError] = useState<any>(null)
+
   const [bankNumber, setBankNumber] = useState<string>(kitchen ? kitchen?.bank_account?.bankNumber : '')
   const [bankNumberError, setBankNumberError] = useState<any>(null)
 
-  const handleChangeSelect = (e: any) => {
-    setBank(e.target.value)
-    setBankError(null)
-  }
+  const [ownerName, setOwnerName] = useState<string>(kitchen ? kitchen?.bank_account?.ownerName : '')
+  const [ownerNameError, setOwnerNameError] = useState<any>(null)
 
-  const handleChangeInput = (e: any) => {
-    setBankNumber(e.target.value)
-    setBankNumberError(null)
-  }
+  const [ownerDocumentType, setOwnerDocumentType] = useState<any>(kitchen ? kitchen?.bank_account?.ownerDocumentType : '')
+  const [ownerDocumentTypeError, setOwnerDocumentTypeError] = useState<any>(null)
+
+  const [ownerDocumentNumber, setOwnerDocumentNumber] = useState<string>(kitchen ? kitchen?.bank_account?.ownerDocumentNumber : '')
+  const [ownerDocumentNumberError, setOwnerDocumentNumberError] = useState<any>(null)
 
   const handleSubmit = (e: any) => {
     e.preventDefault()
-    if (!bank) {
+    if (!accountType) {
+      setAccountTypeError('Selecciona tu tipo de cuenta')
+      return
+    } else if (!bank) {
       setBankError('Selecciona tu banco')
       return
-    }
-    if (bankNumber.length < 9 || bankNumber.length > 20) {
+    } else if (bankNumber.length < 9 || bankNumber.length > 20) {
       setBankNumberError('Cuenta de banco invalida')
+      return
+    } else if (!ownerName) {
+      setOwnerNameError('Ingresa tu nombre')
+      return
+    } else if (!ownerDocumentType) {
+      setOwnerDocumentTypeError('Selecciona tu tipo de documento')
+      return
+    } else if (ownerDocumentNumber.length < 9 || ownerDocumentNumber.length > 20) {
+      setOwnerDocumentNumberError('Documento de propietario invalida')
       return
     }
 
     supabase
       .from('kitchens')
-      .update({ bank_account: { bank, bankNumber } })
+      .update({ bank_account: { accountType, bank, bankNumber, ownerName, ownerDocumentType, ownerDocumentNumber } })
       .eq('id', kitchenId)
       .select('*')
       .then(({ error, data }) => {
-        if (error) {
-          return
-        }
+        if (error) return
         setStore('kitchen', data[0])
         onClose()
       })
@@ -103,8 +69,31 @@ export function BankAccount ({ onClose }: { onClose: any }) {
       <p>Asegurarse de que esta cuenta sea suya, no nos hacemos responsables de cualquier pérdida o daño que pudiese sufrir.</p>
       <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
         <Select
-          label='Selecciona tu cuenta de banco'
-          onChange={handleChangeSelect}
+          label='Selecciona tu tipo de cuenta'
+          onChange={(e: any) => {
+            setAccountType(e.target.value)
+            setAccountTypeError(null)
+          }}
+          defaultSelectedKeys={[accountType]}
+          errorMessage={accountTypeError && accountTypeError}
+          isInvalid={!!accountTypeError}
+        >
+          <SelectItem value='1' key='1'>
+            Ahorros
+          </SelectItem>
+          <SelectItem value='2' key='2'>
+            Corriente
+          </SelectItem>
+          <SelectItem value='3' key='3'>
+            Deposito electrónico
+          </SelectItem>
+        </Select>
+        <Select
+          label='Selecciona tu entidad bancaria'
+          onChange={(e: any) => {
+            setBank(e.target.value)
+            setBankError(null)
+          }}
           errorMessage={bankError && bankError}
           isInvalid={!!bankError}
           defaultSelectedKeys={[bank]}
@@ -122,9 +111,57 @@ export function BankAccount ({ onClose }: { onClose: any }) {
           label='Número de cuenta'
           type='number'
           value={bankNumber}
-          onChange={handleChangeInput}
+          onChange={(e: any) => {
+            setBankNumber(e.target.value)
+            setBankNumberError(null)
+          }}
           errorMessage={bankNumberError && bankNumberError}
           isInvalid={!!bankNumberError}
+        />
+        <Input
+          label='Nombre del propietario'
+          type='text'
+          value={ownerName}
+          onChange={(e: any) => {
+            setOwnerName(e.target.value)
+            setOwnerNameError(null)
+          }}
+          errorMessage={ownerNameError && ownerNameError}
+          isInvalid={!!ownerNameError}
+        />
+        <Select
+          label='Selecciona tu tipo de documento'
+          onChange={(e: any) => {
+            setOwnerDocumentType(e.target.value)
+            setOwnerDocumentTypeError(null)
+          }}
+          defaultSelectedKeys={[ownerDocumentType]}
+          errorMessage={ownerDocumentTypeError && ownerDocumentTypeError}
+          isInvalid={!!ownerDocumentTypeError}
+        >
+          <SelectItem value='1' key='1'>
+            Cédula de ciudadanía
+          </SelectItem>
+          <SelectItem value='2' key='2'>
+            Cédula de extranjería
+          </SelectItem>
+          <SelectItem value='3' key='3'>
+            NIT
+          </SelectItem>
+          <SelectItem value='6' key='6'>
+            Pasaporte
+          </SelectItem>
+        </Select>
+        <Input
+          label='Número de documento del propietario'
+          type='number'
+          value={(ownerDocumentNumber)}
+          onChange={(e: any) => {
+            setOwnerDocumentNumber(e.target.value)
+            setOwnerDocumentNumberError(null)
+          }}
+          errorMessage={ownerDocumentNumberError && ownerDocumentNumberError}
+          isInvalid={!!ownerDocumentNumberError}
         />
         <Button
           type='submit'
--- src/app/settings/bank-account/page.tsx
+++ src/app/settings/bank-account/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { useSupabase } from '@/app/providers'
-import { useState, useEffect, FC } from 'react'
+import { useState, FC } from 'react'
 import { Card, CardHeader, CardBody, Select, SelectItem, Input, Button } from '@nextui-org/react'
 import { Landmark } from 'lucide-react'
 import { useData } from '@/store'
+import { banks } from './banks'
 
 interface ToastProps {
   message: string
@@ -28,61 +29,28 @@ const Toast: FC<ToastProps> = ({ message, isVisible, onClose }) => {
   )
 }
 
-const banks = [
-  {
-    name: 'Bancolombia',
-    value: 'Bancolombia'
-  },
-  {
-    name: 'Banco Caja Social',
-    value: 'Banco Caja Social'
-  },
-  {
-    name: 'Banco Davivienda',
-    value: 'Banco Davivienda'
-  },
-  {
-    name: 'Banco de Bogotá',
-    value: 'Banco de Bogotá'
-  },
-  {
-    name: 'Banco de la República',
-    value: 'Banco de la República'
-  },
-  {
-    name: 'Banco de Occidente',
-    value: 'Banco de Occidente'
-  },
-  {
-    name: 'BBVA Colombia',
-    value: 'BBVA Colombia'
-  },
-  {
-    name: 'Banco Pichincha',
-    value: 'Banco Pichincha'
-  },
-  {
-    name: 'Banco Santander Colombia',
-    value: 'Banco Santander Colombia'
-  },
-  {
-    name: 'Financiera Comultrasan',
-    value: 'Financiera Comultrasan'
-  },
-  {
-    name: 'Itaú Corpbanca Colombia',
-    value: 'Itaú Corpbanca Colombia'
-  }
-]
-
 export default function BankAccount () {
   const { kitchen, kitchenId, setStore } = useData()
   const { supabase } = useSupabase()
 
-  const [bank, setBank] = useState<any>(null)
+  const [accountType, setAccountType] = useState<any>(kitchen ? kitchen?.bank_account?.accountType : '')
+  const [accountTypeError, setAccountTypeError] = useState<any>(null)
+
+  const [bank, setBank] = useState<any>(kitchen ? kitchen?.bank_account?.bank : null)
   const [bankError, setBankError] = useState<any>(null)
-  const [bankNumber, setBankNumber] = useState<string>('')
+
+  const [bankNumber, setBankNumber] = useState<string>(kitchen ? kitchen?.bank_account?.bankNumber : '')
   const [bankNumberError, setBankNumberError] = useState<any>(null)
+
+  const [ownerName, setOwnerName] = useState<string>(kitchen ? kitchen?.bank_account?.ownerName : '')
+  const [ownerNameError, setOwnerNameError] = useState<any>(null)
+
+  const [ownerDocumentType, setOwnerDocumentType] = useState<any>(kitchen ? kitchen?.bank_account?.ownerDocumentType : '')
+  const [ownerDocumentTypeError, setOwnerDocumentTypeError] = useState<any>(null)
+
+  const [ownerDocumentNumber, setOwnerDocumentNumber] = useState<string>(kitchen ? kitchen?.bank_account?.ownerDocumentNumber : '')
+  const [ownerDocumentNumberError, setOwnerDocumentNumberError] = useState<any>(null)
+
   const [toastVisible, setToastVisible] = useState(false)
 
   function showToast () {
@@ -92,48 +60,41 @@ export default function BankAccount () {
     }, 3000)
   }
 
-  const handleChangeSelect = (e: any) => {
-    setBank(e.target.value)
-    setBankError(null)
-  }
-
-  const handleChangeInput = (e: any) => {
-    setBankNumber(e.target.value)
-    setBankNumberError(null)
-  }
-
   const handleSubmit = (e: any) => {
     e.preventDefault()
-    if (!bank) {
+
+    if (!accountType) {
+      setAccountTypeError('Selecciona tu tipo de cuenta')
+      return
+    } else if (!bank) {
       setBankError('Selecciona tu banco')
       return
-    }
-    if (bankNumber.length !== 20) {
+    } else if (bankNumber.length < 9 || bankNumber.length > 20) {
       setBankNumberError('Cuenta de banco invalida')
+      return
+    } else if (!ownerName) {
+      setOwnerNameError('Ingresa tu nombre')
+      return
+    } else if (!ownerDocumentType) {
+      setOwnerDocumentTypeError('Selecciona tu tipo de documento')
+      return
+    } else if (ownerDocumentNumber.length < 9 || ownerDocumentNumber.length > 20) {
+      setOwnerDocumentNumberError('Documento de propietario invalida')
       return
     }
 
     supabase
       .from('kitchens')
-      .update({ bank_account: { bank, bankNumber } })
+      .update({ bank_account: { accountType, bank, bankNumber, ownerName, ownerDocumentType, ownerDocumentNumber } })
       .eq('id', kitchenId)
       .select('*')
       .then(res => {
-        if (res.error) {
-          return
-        }
+        if (res.error) return
+
         setStore('kitchen', res.data[0])
         showToast()
       })
   }
-
-  useEffect(() => {
-    if (kitchen === null) {
-      return
-    }
-    setBank(kitchen.bank_account.bank)
-    setBankNumber(kitchen.bank_account.bankNumber)
-  }, [kitchen])
 
   return (
     <Card className='w-96'>
@@ -151,11 +112,34 @@ export default function BankAccount () {
       <CardBody>
         <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
           <Select
-            selectedKeys={[bank]}
-            label='Selecciona tu cuenta de banco'
-            onChange={handleChangeSelect}
+            label='Selecciona tu tipo de cuenta'
+            onChange={(e: any) => {
+              setAccountType(e.target.value)
+              setAccountTypeError(null)
+            }}
+            defaultSelectedKeys={[accountType]}
+            errorMessage={accountTypeError && accountTypeError}
+            isInvalid={!!accountTypeError}
+          >
+            <SelectItem value='1' key='1'>
+              Ahorros
+            </SelectItem>
+            <SelectItem value='2' key='2'>
+              Corriente
+            </SelectItem>
+            <SelectItem value='3' key='3'>
+              Deposito electrónico
+            </SelectItem>
+          </Select>
+          <Select
+            label='Selecciona tu entidad bancaria'
+            onChange={(e: any) => {
+              setBank(e.target.value)
+              setBankError(null)
+            }}
             errorMessage={bankError && bankError}
             isInvalid={!!bankError}
+            defaultSelectedKeys={[bank]}
           >
             {banks.map(bank => (
               <SelectItem
@@ -170,9 +154,57 @@ export default function BankAccount () {
             label='Número de cuenta'
             type='number'
             value={bankNumber}
-            onChange={handleChangeInput}
+            onChange={(e: any) => {
+              setBankNumber(e.target.value)
+              setBankNumberError(null)
+            }}
             errorMessage={bankNumberError && bankNumberError}
             isInvalid={!!bankNumberError}
+          />
+          <Input
+            label='Nombre del propietario'
+            type='text'
+            value={ownerName}
+            onChange={(e: any) => {
+              setOwnerName(e.target.value)
+              setOwnerNameError(null)
+            }}
+            errorMessage={ownerNameError && ownerNameError}
+            isInvalid={!!ownerNameError}
+          />
+          <Select
+            label='Selecciona tu tipo de documento'
+            onChange={(e: any) => {
+              setOwnerDocumentType(e.target.value)
+              setOwnerDocumentTypeError(null)
+            }}
+            defaultSelectedKeys={[ownerDocumentType]}
+            errorMessage={ownerDocumentTypeError && ownerDocumentTypeError}
+            isInvalid={!!ownerDocumentTypeError}
+          >
+            <SelectItem value='1' key='1'>
+              Cédula de ciudadanía
+            </SelectItem>
+            <SelectItem value='2' key='2'>
+              Cédula de extranjería
+            </SelectItem>
+            <SelectItem value='3' key='3'>
+              NIT
+            </SelectItem>
+            <SelectItem value='6' key='6'>
+              Pasaporte
+            </SelectItem>
+          </Select>
+          <Input
+            label='Número de documento del propietario'
+            type='number'
+            value={(ownerDocumentNumber)}
+            onChange={(e: any) => {
+              setOwnerDocumentNumber(e.target.value)
+              setOwnerDocumentNumberError(null)
+            }}
+            errorMessage={ownerDocumentNumberError && ownerDocumentNumberError}
+            isInvalid={!!ownerDocumentNumberError}
           />
           <Button
             type='submit'
